cff3c5611ff0e935c45b02f3d9d863aadd7e116f
Philipp Bode <philipp.bode@sap.com>
2018-02-15 21:02:33 -0800

    Initial commit.

---
+++ app.py
@@ -0,0 +1,19 @@
+from datetime import datetime
+from flask import Flask
+app = Flask(__name__)
+
+
+@app.route('/')
+def homepage():
+    the_time = datetime.now().strftime("%A, %d %b %Y %l:%M %p")
+
+    return """
+    <h1>Hello heroku</h1>
+    <p>It is currently {time}.</p>
+
+    <img src="http://loremflickr.com/600/400">
+    """.format(time=the_time)
+
+
+if __name__ == '__main__':
+    app.run(debug=True, use_reloader=True)
